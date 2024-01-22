1133dfac425f73eef54554190396ac0055e15a9b
Mohamed Aziz Bechaib <azizbechaib711@gmail.com>
2024-01-22 14:02:03 +0100

    FIX: data format fix in the model

--- src/__test__/setupFile.ts
+++ src/__test__/setupFile.ts
@@ -6,46 +6,19 @@ beforeAll(async () => {
 
     const checklist1 = Checklist.build({
         organe: "Organe",
-        operations: [
-            {
-                operation: "Operation 1",
-                type: "valid",
-            },
-            {
-                operation: "Operation 2",
-                type: "ok",
-            },
-        ],
+        operations: ["Operation 1", "Operation 2"],
     });
     await checklist1.save();
 
     const checklist2 = Checklist.build({
         organe: "Another Organe",
-        operations: [
-            {
-                operation: "Another Operation 1",
-                type: "valid",
-            },
-            {
-                operation: "Another Operation 2",
-                type: "ok",
-            },
-        ],
+        operations: ["Another Operation 1", "Another Operation 2"],
     });
     await checklist2.save();
 
     const checklist3 = Checklist.build({
         organe: "Yet ANother Organe",
-        operations: [
-            {
-                operation: "Yet ANother Operation 1",
-                type: "valid",
-            },
-            {
-                operation: "Yet ANother Operation 2",
-                type: "ok",
-            },
-        ],
+        operations: ["Yet ANother Operation 1", "Yet ANother Operation 2"],
     });
     await checklist3.save();
 });
