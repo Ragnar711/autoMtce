850d43192dccd27957c895c7434a65c5a533cd2d
Mohamed Aziz Bechaib <azizbechaib711@gmail.com>
2024-01-23 12:04:04 +0100

    FEAT: add-checklist + passing test

--- src/Routes/checklist-routes.ts
+++ src/Routes/checklist-routes.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { getChecklistHandler } from "../Controllers/get-checklist";
 import { updateChecklistHandler } from "../Controllers/update-checklist";
 import { deleteChecklistHandler } from "../Controllers/delete-checklist";
+import { addChecklistHandler } from "../Controllers/add-checklist";
 
 const router = Router();
 
 router.get("/", getChecklistHandler);
 router.put("/:id", updateChecklistHandler);
 router.put("/delete/:id", deleteChecklistHandler);
+router.post("/", addChecklistHandler);
 
 export default router;
